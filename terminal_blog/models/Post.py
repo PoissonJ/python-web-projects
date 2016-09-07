434bbcb58b22df61a005a66e37c5b9fb589ca594
Jonathan Poisson <jonathan.poisson@summitholdings.com>
2016-09-07 12:39:50 -0400

    Added uuid generation support

--- terminal_blog/models/Post.py
+++ terminal_blog/models/Post.py
@@ -1,14 +1,15 @@
+import uuid
 from Database import Database
 
 class Post(object):
 
-    def __init__(self, blog_id, title, content, author, id, data):
+    def __init__(self, blog_id, title, content, author, data, id=None):
         self.blog_id = blog_id
         self.title = title
         self.content = content
         self.author = author
         self.created_date = date
-        self.id = id
+        self.id = uuid.uuid4().hex if id is None else id
 
     def save_to_mongo(self):
         Database.insert(collection='posts', data=self.json())
