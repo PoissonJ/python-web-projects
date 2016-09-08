ad0a1c59a33353956a10fb263d7cc5abc3758b6a
Jonathan Poisson <jonathan.poisson@summitholdings.com>
2016-09-08 10:15:58 -0400

    Finished Menu class

--- terminal_blog/Menu.py
+++ terminal_blog/Menu.py
@@ -15,16 +15,17 @@ class Menu(object):
     def run_menu(self):
         read_or_write = input("Do you want to (R) or write (W) blogs?")
         if read_or_write == 'R':
-            pass
+            self._list_blogs()
+            self._view_blog()
         elif read_or_write == 'W':
-            pass
+            self.user_blog.new_post()
         else:
             print("Thank you for blogging!")
 
     def _user_has_account(self):
         blog = Database.find_one('blogs', {'author': self.user})
         if blog is not None:
-            self.user_blog = blog
+            self.user_blog = Blog.get_from_mongo(blog['id'])
             return True
         return False
 
@@ -34,3 +35,21 @@ class Menu(object):
         blog = Blog(self.user, self.title, self.description)
         blog.save_to_mongo()
         self.user_blog = blog
+
+    def _list_blogs(self):
+        blogs = Database.find(collection='blogs', query={})
+        for blog in blogs:
+            print 'ID: ', blog['id']
+            print 'Title: ', blog['title']
+            print 'Author: ', blog['author']
+            print ''
+
+    def _view_blog(self):
+        blog_to_see = input("Enter the ID of the blog you'd like to read: ")
+        blog = Blog.get_from_mongo(blog_to_see)
+        posts = blog.get_posts()
+        for post in posts:
+            print 'Date: ', post['created_date']
+            print 'Title: ', post['title']
+            print ''
+            print post['title']
